fb09755feeec212802b26e4b68549869a8bd28bc
Juan Manuel Lopez <tandiljuan@gmail.com>
2024-03-27 22:41:40 -0300

    Build WordPress (FrankenPHP) image

--- provision/images.sh
+++ provision/images.sh
@@ -6,3 +6,9 @@ docker image pull ubuntu:jammy
 docker image pull mariadb:10.11
 docker image pull redis:7.2-bookworm
 docker image pull dunglas/frankenphp:1.1.1-php8.2-bookworm
+
+# Build FrankenPHP image for WordPress
+cd /home/vagrant/dockerfiles/frankenphp
+docker build --rm \
+    --tag 'frankenphp/wordpress' \
+    .
